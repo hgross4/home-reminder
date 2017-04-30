8c66d70fd92061e03ce2dc2520b1fd6b6fb072df
Brent <woolvish-computing@yahoo.com>
2017-04-30 18:03:36 -0500

    Add files via upload

--- hey google set reminder.sh
+++ hey google set reminder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {1..5000}
 do
-   afplay /Users/woolvish-miscshopping/Documents/hey\ google\ set\ reminder.aifc
+   afplay /Users/woolvish-miscshopping/Documents/hey\ google\ set\ reminder\ shorter,\ louder.aiff
    sleep 1
 done
